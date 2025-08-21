777ca592bad4e42f2c7533fdfae6b7dac5af3b87
Prasun Maji <prasunmajismk@gmail.com>
2025-08-21 18:20:02 +0530

    Interview Doctype Changes

--- job_club/job_club/doctype/interview/interview.js
+++ job_club/job_club/doctype/interview/interview.js
@@ -21,11 +21,11 @@ frappe.ui.form.on('Interview', {
                 email_id: frm.doc.email_id,
                 mobile_no: frm.doc.mobile_number,
                 lead_name: frm.doc.full_name,
-                city: frm.doc.custom_city,
-                gender: frm.doc.custom_gender,
-                age: frm.doc.custom_age,
-                height: frm.doc.custom_height,
-                qualification: frm.doc.custom_qualification
+                city: frm.doc.city,
+                gender: frm.doc.gender,
+                age: frm.doc.age,
+                height: frm.doc.height,
+                qualification: frm.doc.qualification
             });
         }, 'Create');
     }
