7257683fdee6de9fe35e91399b0142ab10675ee2
Prasun Maji <prasunmajismk@gmail.com>
2025-09-05 17:56:10 +0530

    Some Changes in Registration and Admission Form

--- job_club/job_club/web_form/admission_form/admission_form.js
+++ job_club/job_club/web_form/admission_form/admission_form.js
@@ -350,7 +350,7 @@ frappe.ready(() => {
 <!-- Mobile Header -->
 <div class="wave-header">
     <img src="https://www.emporiumsolutions.com/wp-content/uploads/2025/07/logo-erp.png" class="logo" />
-    <h2>Free Career Counselling</h2>
+    <h2>Admission Form</h2>
     <img src="https://www.emporiumsolutions.com/wp-content/uploads/2025/07/2girl.png" class="girls-img" />
     <svg class="wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#fff" fill-opacity="1" d="M0,192L1440,96L1440,320L0,320Z"></path>
