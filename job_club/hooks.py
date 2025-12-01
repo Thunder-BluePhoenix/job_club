9cdbf4c9a9f1fdb168104a5f0ea10390e4104a5b
Prasun Maji <prasunmajismk@gmail.com>
2025-12-02 01:23:11 +0530

    Hide Help

--- job_club/hooks.py
+++ job_club/hooks.py
@@ -28,6 +28,9 @@ app_license = "mit"
 # app_include_css = "/assets/job_club/css/job_club.css"
 # app_include_js = "/assets/job_club/js/job_club.js"
 
+app_include_css = "/assets/job_club/css/hide_help.css"
+
+
 # include js, css files in header of web template
 # web_include_css = "/assets/job_club/css/job_club.css"
 # web_include_js = "/assets/job_club/js/job_club.js"
